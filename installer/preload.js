5a282db16a83147a255957f0718704d589472942
owen307 <owen@theyarnells.com>
2026-06-13 07:14:27 -0600

    Fix: add browseISO to preload

--- installer/preload.js
+++ installer/preload.js
@@ -1,10 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('api', {
   getSysinfo:      ()       => ipcRenderer.invoke('get-sysinfo'),
-  runInstall:      (opts)   => ipcRenderer.invoke('run-install', opts),
+  scanDrives:      ()       => ipcRenderer.invoke('scan-drives'),
+  writeUSB:        (opts)   => ipcRenderer.invoke('write-usb', opts),
+  cancelWrite:     ()       => ipcRenderer.invoke('cancel-write'),
+  requestAdmin:    ()       => ipcRenderer.invoke('request-admin'),
+  browseISO:       ()       => ipcRenderer.invoke('browse-iso'),
   minimize:        ()       => ipcRenderer.send('minimize'),
   close:           ()       => ipcRenderer.send('close'),
   openUrl:         (url)    => ipcRenderer.send('open-url', url),
-  onProgress:      (cb)     => ipcRenderer.on('install-progress', (_, data) => cb(data))
+  onWriteProgress: (cb)     => ipcRenderer.on('write-progress', (_, d) => cb(d)),
 });
